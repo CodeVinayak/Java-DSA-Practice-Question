fe1710273b6a28dec1a5b9b140424efdeba63e04
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-03-19 00:29:29 +0530

    Create 09 Sort The Array.java

---
+++ Arrays/09 Sort The Array.java
@@ -0,0 +1,12 @@
+// url: https://www.geeksforgeeks.org/problems/sort-the-array0055/1?page=2&difficulty=Basic&sortBy=submissions
+
+class Solution 
+{ 
+    int[] sortArr(int[] arr, int n) 
+    { 
+        Arrays.sort(arr);
+        return arr;
+    }
+} 
+//Time complexity: O(nlogn)
+// Space complexity: O(n)
