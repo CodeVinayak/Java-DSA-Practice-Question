a0e77dcc84ec91ad2a1f065996e45bd54b007f00
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-03-24 15:43:28 +0530

    Create 18 Sort an array of 0s, 1s and 2s.java

---
+++ Arrays/18 Sort an array of 0s, 1s and 2s.java
@@ -0,0 +1,15 @@
+// url: https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/0
+
+// Not Optimized
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        Arrays.sort(a);
+    }
+}
+
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
