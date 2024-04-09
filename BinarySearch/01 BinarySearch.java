710a9232baf06afa78b11f6b52e0cdc932174d31
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-04-09 19:54:16 +0530

    Create 01 BinarySearch.java

---
+++ BinarySearch/01 BinarySearch.java
@@ -0,0 +1,22 @@
+// url: https://leetcode.com/problems/binary-search/submissions/1227701369/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
