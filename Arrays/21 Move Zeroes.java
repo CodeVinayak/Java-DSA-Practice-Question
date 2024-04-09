29de0da56971d5e8148aec19048b16118570180c
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-04-09 19:33:36 +0530

    Create 21 Move Zeroes.java

---
+++ Arrays/21 Move Zeroes.java
@@ -0,0 +1,25 @@
+// url: https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+    public void moveZeroes(int[] nums) 
+    {
+        int n=nums.length;
+        int nonZeroIndex=0;
+        // Move all non-zero elements to the front
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[nonZeroIndex]=nums[i];
+                nonZeroIndex++;
+            }
+        }
+        // Fill the remaining elements with 0
+        while(nonZeroIndex<n)
+        {
+            nums[nonZeroIndex]=0;
+            nonZeroIndex++;
+        }
+    }
+}
+
+// TimeComplexity=O(n)
+// SPaceComplexity=O(1)
